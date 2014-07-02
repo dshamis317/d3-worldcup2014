0b7044abf1caaa43516878580ad4dcec6f829641
dshamis317 <dshamis317@gmail.com>
2014-07-02 17:42:56 -0400

    moved index.html script files to visualization.js

---
+++ public/visualization.js
@@ -0,0 +1,38 @@
+$(function() {
+    $('.team-submit').submit(function(e) {
+      e.preventDefault();
+      teamData();
+    })
+  });
+
+  function teamData() {
+    var inputField = $('.team-name');
+    var team = inputField.val().toUpperCase();
+    inputField.val('');
+    postTeam(team);
+  };
+
+  function postTeam(team) {
+    $.ajax({
+      url: '/team',
+      method: 'post',
+      dataType: 'json',
+      data: {teamName: team},
+      success: function(data) {
+        // console.log(data);
+        getTeamData(data.teamName);
+      }
+    });
+  }
+
+  function getTeamData(team) {
+    var teamName = team;
+    $.ajax({
+      url: '/team/'+ teamName,
+      method: 'get',
+      // dataType: 'json',
+      success: function(data) {
+        console.log(data)
+      }
+    });
+  }
